4c156bb4f482e833762332b554ee345ff5c3c1ca
haved <krogstie.havard@gmail.com>
2013-12-02 19:01:29 +0100

    Finish basic movingEntity

--- MyEngine/src/me/haved/engine/entity/MovingEntity.java
+++ MyEngine/src/me/haved/engine/entity/MovingEntity.java
@@ -13,6 +13,13 @@ public class MovingEntity extends Entity
 	
 	public void move(World world)
 	{
-		slowX = xSpeed * Time.delta();
+		slowX += xSpeed * Time.delta();
+		slowY += ySpeed * Time.delta();
+		
+		x += (slowX / 1000);
+		y += (slowY / 1000);
+		
+		slowX = slowX % 1000;
+		slowY = slowY % 1000;
 	}
 }
