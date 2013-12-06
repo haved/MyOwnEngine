ea17ff112d2fc4d9d2599e2f84b822255168516a
haved <krogstie.havard@gmail.com>
2013-12-06 17:34:34 +0100

    Made the MyUIButton functional!

--- MyGame/src/me/haved/game/menu/MyUIButton.java
+++ MyGame/src/me/haved/game/menu/MyUIButton.java
@@ -1,7 +1,9 @@
 package me.haved.game.menu;
 
 import me.haved.engine.entity.Entity;
+import me.haved.engine.lib.Input;
 import me.haved.engine.render.RenderEngine;
+import me.haved.engine.world.World;
 
 public class MyUIButton extends Entity
 {
@@ -9,11 +11,62 @@ public class MyUIButton extends Entity
 	private String text;
 	private String action;
 	
+	private boolean mouseOver;
+	private boolean buttonDown;
+	private boolean prevMouseState;
+	
+	public MyUIButton()
+	{
+		width = 300;
+		height = 40;
+	}
+	
+	@Override
+	public void update(World world)
+	{
+		int mX = Input.getMouseX();
+		int mY = Input.getMouseY();
+		
+		mouseOver = mX > getX() & mX < getX() + getWidth() & mY > getY() & mY < getY() + getHeight();
+		
+		if(Input.isLMB() & mouseOver & prevMouseState == false)
+		{
+			buttonDown = true;
+		}
+		
+		if(buttonDown & Input.isLMB() == false)
+		{
+			buttonDown = false;
+			if(mouseOver)
+			{
+				activate(world);
+			}
+		}
+		
+		prevMouseState = Input.isLMB();
+	}
+	
 	@Override
 	public void render()
 	{
-		RenderEngine.setColor4f(1.0f, 0.7f, 0.4f, 1);
+		if(mouseOver & buttonDown)
+		{
+			RenderEngine.setColor4f(1.0f, 1f, 0.5f, 1);
+		}
+		else if(mouseOver)
+		{
+			RenderEngine.setColor4f(1.0f, 0.8f, 0.6f, 1);
+		}
+		else
+		{
+			RenderEngine.setColor4f(1.0f, 0.7f, 0.4f, 1);
+		}
 		RenderEngine.drawRect(getX(), getY(), getWidth(), getHeight());
+	}
+	
+	protected void activate(World world)
+	{
+		
 	}
 	
 	public int getWidth()
